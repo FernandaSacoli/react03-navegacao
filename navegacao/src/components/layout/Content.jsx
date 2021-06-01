049f892fd2ebbaa92bd3e6156753789d869ec645
FernandaSacoli <fersacoli@gmail.com>
2021-06-01 23:19:20 +0000

    Commit 01

---
+++ navegacao/src/components/layout/Content.jsx
@@ -0,0 +1,14 @@
+import './Content.css'
+import React from "react"
+
+const Content = propd => (
+
+    <main className="Content">
+
+    <h1>Apresentação dos Contéudos</h1>
+
+    </main>
+
+)
+
+export default Content
---
+++ navegacao/src/components/layout/Menu.jsx
@@ -0,0 +1,30 @@
+import './Menu.css'
+import React from 'react'
+
+const Menu = props => (
+    <aside className="Menu">
+
+        <nav>
+            <ul>
+    <li>
+        <a href="/">Home</a>
+    </li>
+    <li>
+    <a href="/">Conteúdo 01 </a>
+    </li>
+    <li> 
+    <a href="/">Conteúdo 02 </a>
+    </li>
+    <li> 
+    <a href="/"> Conteúdo 03 </a>
+    </li>
+    <li> 
+    <a href="/">  Conteúdo 04 </a>
+    </li>
+            </ul>
+        </nav>
+
+    </aside>
+)
+
+export default Menu
